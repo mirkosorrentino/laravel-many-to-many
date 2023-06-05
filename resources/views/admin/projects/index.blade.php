a73e5f0d330e794574536afbb3ebda6994146d3d
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-06-05 18:27:53 +0200

    Add: delete function

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
 <h1>Lista dei Progetti</h1>
+<div class="text-end">
+    <a href="{{ route('admin.projects.create') }}" class="btn btn-primary">
+        <i class="fa-solid fa-square-plus"></i> Aggiungi un nuovo progetto
+    </a>
+</div>
 
 <table class="table">
     <thead>
@@ -9,7 +14,7 @@
             <th scope="col">Id</th>
             <th scope="col">Titolo</th>
             <th scope="col">Slug</th>
-            <th scope="col">Actions</th>
+            <th scope="col">Actions</th> 
         </tr>
     </thead>
     <tbody>
@@ -22,9 +27,13 @@
                     <a href="{{ route('admin.projects.show', $project->slug) }}" class="btn btn-success">
                         <i class="fa-solid fa-eye"></i>
                     </a>
-                    <a href="{{ route('admin.projects.create', $project->slug) }}" class="btn btn-primary">
-                        <i class="fa-solid fa-square-plus"></i>
-                    </a>
+                    <form class="d-inline-block" action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit" class="btn btn-danger">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </form>
                 </td>
             </tr>
         @endforeach
